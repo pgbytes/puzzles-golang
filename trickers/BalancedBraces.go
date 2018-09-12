36c0b6286b86dfde393db18629200711acbc8905
panshul.gupta <panshul.gupta@masterpayment.com>
2018-09-12 17:09:51 +0200

    add: code to check if given expression has balanced braces.

--- trickers/BalancedBraces.go
+++ trickers/BalancedBraces.go
@@ -1,9 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"puzzles-golang/ds"
+)
+
 const sampleBalanced = "{[()()][]}"
 const sampleBalancedImpure = "{[(3)(a+b)][]}"
 const sampleUnbalanced = "{[()}"
 
 func main() {
+	fmt.Printf("%s -> %t\n", sampleBalanced, isBalanced(sampleBalanced))
+	fmt.Printf("%s -> %t\n", sampleBalancedImpure, isBalanced(sampleBalancedImpure))
+	fmt.Printf("%s -> %t\n", sampleUnbalanced, isBalanced(sampleUnbalanced))
+}
 
+func isBalanced(sample string) bool {
+	braceStack := ds.NewStack()
+	sampleRune := []rune(sample)
+	for i := 0; i < len(sampleRune); i++ {
+		brace := string(sampleRune[i])
+		// Check for opening braces
+		if brace == "{" || brace == "[" || brace == "(" {
+			braceStack.Push(string(brace))
+		}
+		// Check for closing braces
+		if brace == "}" || brace == "]" || brace == ")" {
+			opening, err := braceStack.Pop()
+			// Check if stack is empty.. hence no opening braces
+			if err != nil {
+				log.Fatalln(err)
+				return false
+			}
+			if (opening == "{" && brace != "}") || (opening == "[" && brace != "]") || (opening == "(" && brace != ")") {
+				return false
+			}
+		}
+	}
+	return braceStack.IsEmpty()
 }
